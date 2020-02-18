6b6712230c6abf51a00605b83054b727caae3e41
Mally13 <mallymutuku13@gmail.com>
2020-02-19 01:21:55 +0300

    new

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class App extends Component {
+  name='mally'
+  render(){
+   return (
+    <div className="App">
+    React
+    {this.name}
+    </div>
+  );
+}
+}
+
+export default App;
